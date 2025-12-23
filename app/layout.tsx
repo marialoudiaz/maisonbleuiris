013f07bbef26a2698cdf87b2967471dda3aa4997
marialoudiaz <loumariadiaz@gmail.com>
2025-12-24 00:05:41 +0100

    seo local

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import '../app/globals.css';
 //Développment d’image de marque pour tous ceux qui promeut une philosophie bien-être, éco-responsable et éthique: lieux, marques, artisans, praticiens, etc…
 export const metadata = {
   title: {
-    default: "Maison Bleu Iris",
+    default: "Maison Bleu Iris | Studio graphisme et design de marque Montpellier",
     template: "Studio graphisme et design de marque Montpellier : stratégie marketing, print, web, packaging - Maison Bleu Iris",
   },
   description: "Vous avez un projet ou une marque à révéler ? Maison Bleu Iris accompagne celles et ceux qui défendent une vision éco-responsable et éthique, à travers une approche globale du design de marque, du print au digital: Print, Développement web, Illustration, Packaging", //Studio expert en développement d’image de marque pour tous ceux qui promeut une philosophie éco-responsable et éthique.
