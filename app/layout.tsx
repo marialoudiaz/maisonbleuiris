62f6e89f4d23f86aa88d66657bf5b489c46a8b9d
marialoudiaz <loumariadiaz@gmail.com>
2025-12-26 19:16:39 +0100

    eviter ralentissement LCP

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { DataProvider } from './context/DataContext';
 // import { Providers } from "./providers";
 import '../app/globals.css';
+// import { Barlow, Barlow_Condensed} from 'next/font/google';
+import Script from 'next/script';
+
 
 
 // export const metadata = {
@@ -15,7 +18,6 @@ export const metadata = {
     template: "Maison Bleu Iris | Studio graphisme et design de marque Montpellier", //: stratégie marketing, print, web, packaging
   },
   description: "Vous avez un projet ou une marque à révéler ? Maison Bleu Iris accompagne celles et ceux qui défendent une vision éco-responsable et éthique, à travers une approche globale du design de marque, du print au digital: Print, Développement web, Illustration, Packaging", //Studio expert en développement d’image de marque pour tous ceux qui promeut une philosophie éco-responsable et éthique.
-  
   //rs
   openGraph: {
     title: "Maison Bleu Iris | Studio graphisme et design de marque Montpellier",//: stratégie marketing, print, web, packaging
@@ -39,9 +41,38 @@ export const metadata = {
   //fin rs
 };
 
+/// Charger Barlow sans retarder le LCP
+// export const barlow = Barlow({
+//   subsets: ['latin'],
+//   weight: ['100','200','300','400','500','600','700','800','900'],
+//   display: 'swap',
+//   variable: '--font-barlow',
+// });
+
+// export const barlowCondensed = Barlow_Condensed({
+//   subsets: ['latin'],
+//   weight: ['100'],
+//   display: 'swap',
+//   variable: '--font-barlow-condensed',
+// });
+
+
+// a mettre dans html : className={`${barlow.variable} ${barlowCondensed.variable}`}
 export default function RootLayout({children,}:{children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        {/* Charger le kit Adobe Typekit de manière non bloquante */}
+        <Script
+          src="https://use.typekit.net/auw7joq.js"
+          strategy="afterInteractive"
+        />
+        <Script id="typekit-init" strategy="afterInteractive">
+          {`try{Typekit.load({ async: true }); } catch(e){ }`}
+        </Script>
+      </head>
+
+
       <body>
         <main>
           <DataProvider>
